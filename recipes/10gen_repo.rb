77cd7092d0599025d2cc2e60403a71cdbd8c2de8
Oleg Dolya <oleg.dolya@gmail.com>
2012-08-17 16:30:14 +0300

    modify repo

--- recipes/10gen_repo.rb
+++ recipes/10gen_repo.rb
@@ -27,17 +27,17 @@ case node['platform']
     # Adds the repo: http://www.mongodb.org/display/DOCS/Ubuntu+and+Debian+packages
     execute "apt-get update" do
       action :nothing
-   end
+    end
 
-  apt_repository "10gen" do
-    uri "http://downloads-distro.mongodb.org/repo/debian-sysvinit"
-    distribution "dist"
-    components ["10gen"]
-    keyserver "keyserver.ubuntu.com"
-    key "7F0CEB10"
-    action :add
-    notifies :run, "execute[apt-get update]", :immediately
-  end
+    apt_repository "10gen" do
+      uri "http://downloads-distro.mongodb.org/repo/debian-sysvinit"
+      distribution "dist"
+      components ["10gen"]
+      keyserver "keyserver.ubuntu.com"
+      key "7F0CEB10"
+      action :add
+     notifies :run, "execute[apt-get update]", :immediately
+    end
 
     package "mongodb" do
       package_name "mongodb-10gen"
@@ -45,21 +45,30 @@ case node['platform']
   when "redhat","oracle","centos","fedora","suse", "amazon", "scientific"
       arch = node["kernel"]["machine"]
       arch = "i386" unless arch == "x86_64"
-      #/etc/yum.repos.d/10gen.repo
       #http://docs.mongodb.org/manual/tutorial/install-mongodb-on-redhat-centos-or-fedora-linux/
 
-      rpm_file = "percona-release-0.0-1.#{arch}.rpm"
-
-      remote_file "/var/tmp/#{rpm_file}" do
-        source "http://www.percona.com/downloads/percona-release/#{rpm_file}"
-        owner  "root"
-        mode   0644
+      execute "yum -q makecache" do
+        action :nothing
       end
 
-      package "mongo-10gen mongo-10gen-server" do
-        source "/var/tmp/#{rpm_file}"
-        options "--nogpgcheck"
+      ruby_block("reload-yum-cache") do
+        block do
+          Chef::Provider::Package::Yum::YumCache.instance.reload
+        end
       end
+
+      template "/etc/yum.repos.d/10gen.repo" do
+        mode "0644"
+        source "10gen.repo.erb"
+        notifies :run, resources(:execute => "yum -q makecache"), :immediately
+        notifies :create, resources('ruby_block[reload-yum-cache]'), :immediately
+        not_if { ::FileTest.exists?("/etc/yum.repos.d/10gen.repo") }
+      end
+
+      #package "mongo-10gen mongo-10gen-server" do
+      #  source "/var/tmp/#{rpm_file}"
+      #  options "--nogpgcheck"
+      #end
 else
     Chef::Log.warn("Adding the #{node['platform']} 10gen repository is not yet not supported by this cookbook")
 end
